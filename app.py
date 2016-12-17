18071ae1cb7f8549ba9d3a8eaa9a2510c770e2b8
John McKim <johncmckim@gmail.com>
2016-12-17 18:26:17 +1000

    Measure temp every minute

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
+import time
 import grovepi
-# from grove_rgb_lcd import *
 
-dht_sensor_port = 7
-dht_sensor_type = 0
+DHT_SENSOR_PORT = 7
+DHT_SENSOR_TYPE = 0
 
 while True:
-	try:
-		[ temp,hum ] = grovepi.dht(dht_sensor_port,dht_sensor_type)		#Get the temperature and Humidity from the DHT sensor
-		print("temp =", temp, "C\thumidity =", hum,"%") 	
-		# t = str(temp)
-		# h = str(hum)
-		
-		# setRGB(0,128,64)
-		# setRGB(0,255,0)
-		# setText("Temp:" + t + "C      " + "Humidity :" + h + "%")			
-	except (IOError,TypeError) as e:
-		print("Error")
+    try:
+         # Get the temperature and Humidity from the DHT sensor
+        [TEMP, HUM] = grovepi.dht(DHT_SENSOR_PORT, DHT_SENSOR_TYPE)
+        print("temp=", TEMP, "thumidity=", HUM, "%")
+        time.sleep(60)
+    except (IOError, TypeError) as err:
+        print "Error"
